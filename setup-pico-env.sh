c2fe07e431f3dbcb3c4374aa1f66f391d275804e
Tasso <tasso@tassoevan.me>
2024-12-15 17:37:41 -0300

    Add `clean` to justfile

--- setup-pico-env.sh
+++ setup-pico-env.sh
@@ -243,6 +243,9 @@ build:
   @cd build && cmake ..
   @cd build && make
 
+clean:
+  @rm -rf build
+
 upload:
   openocd -f interface/cmsis-dap.cfg -c "adapter speed 5000" -f target/rp2040.cfg -c "program build/$PROJECT_NAME.elf verify reset exit"
 
